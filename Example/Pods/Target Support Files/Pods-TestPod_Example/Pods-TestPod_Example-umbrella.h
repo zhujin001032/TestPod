37d376c0322b21117451b3adf6434b91a7544fd8
zhujin001032 <zhujin001xb@163.con>
2016-03-16 15:19:30 +0800

    测试pod发布

---
+++ Example/Pods/Target Support Files/Pods-TestPod_Example/Pods-TestPod_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TestPod_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TestPod_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/TestPod/TestPod-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double TestPodVersionNumber;
+FOUNDATION_EXPORT const unsigned char TestPodVersionString[];
+
